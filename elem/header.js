09683aa765e6e0cd0023bb6554752359a92e00fe
PapaZote <64790281+PapaZote@users.noreply.github.com>
2020-05-20 14:42:26 -0500

    Add files via upload
    Commit index.html as main page

---
+++ elem/header.js
@@ -0,0 +1,37 @@
+document.write('\
+\
+	<link rel="stylesheet" href="../styles/style.css">\
+	<div class="baseheader">\
+		<div class="divlogo">\
+			<a href="index.html"><img src="imag/logo.png"></a>\
+		</div>\
+		<div class="opcionesmenu">\
+\
+			<!--Aquí comienzan las actividades -->\
+\
+			<a href="actividades.html">\
+				<div>Actividades</div>\
+				<div class="barmenu"></div>\
+			</a>\
+\
+			<a href="pabellon.html">\
+				<div>Pabellón</div>\
+				<div class="barmenu"></div>\
+			</a>\
+\
+			<a href="galeria.html">\
+				<div>Galería</div>\
+				<div class="barmenu"></div>\
+			</a>\
+\
+			<a href="inscribirse.html">\
+				<div>Inscribirse</div>\
+				<div class="barmenu"></div>\
+			</a>\
+\
+			<!--Aquí terminan las actividades -->\
+\
+		</div>\
+	</div>\
+\
+');
---
+++ elem/main.js
@@ -0,0 +1,104 @@
+$(document).ready(function(){
+
+
+	$('ul.list-tabs li a:first').addClass('active');
+	$('.container-act-desc article').hide();
+	$('.container-act-desc article:first').show();
+
+	$('ul.list-tabs li a').click(function(){
+		$('ul.list-tabs li a').removeClass('active');
+		$(this).addClass('active');
+		$('.container-act-desc article').hide();
+
+		var activeTab = $(this).attr('href');
+		$(activeTab).show();
+		return false;
+	});
+
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+  $('#btnSend').click(function(){
+
+        var errores = '';
+
+        // Validado Nombre ==============================
+        if( $('#names').val() == '' ){
+            errores += '<p>Escriba un nombre</p>';
+            $('#names').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#names').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Correo ==============================
+        if( $('#email').val() == '' ){
+            errores += '<p>Ingrese un correo</p>';
+            $('#email').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#email').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Mensaje ==============================
+        if( $('#mensaje').val() == '' ){
+            errores += '<p>Escriba un mensaje</p>';
+            $('#mensaje').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#mensaje').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // ENVIANDO MENSAJE ============================
+        if( errores == '' == false){
+            var mensajeModal = '<div class="modal_wrap">'+
+                                    '<div class="mensaje_modal">'+
+                                        '<h3>Errores encontrados</h3>'+
+                                        errores+
+                                        '<span id="btnClose">Cerrar</span>'+
+                                    '</div>'+
+                                '</div>'
+
+            $('body').append(mensajeModal);
+        }
+
+        // CERRANDO MODAL ==============================
+        $('#btnClose').click(function(){
+            $('.modal_wrap').remove();
+        });
+    });
+
+
+
+  });
